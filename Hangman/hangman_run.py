89b1a8479db59c2689327b94d36c8c897651c96d
BlueMarth <hubert.lcr@gmail.com>
2024-04-29 12:59:36 +0800

    Update hangman_run.py

--- Hangman/hangman_run.py
+++ Hangman/hangman_run.py
@@ -5,14 +5,17 @@ import math
 pg.init()
 
 SCREEN_SIZE = (600,400)
-STAGE_DELAY = 1
-STROKE_DELAY = 1
+STAGE_DELAY = 10
+STROKE_DELAY = 5
+STAGE = 0
 
 STROKE_SIZE = 2
 CHALK_COLOR = "whitesmoke"
 BOARD_COLOR = (39,76,67)
 DONE_DRAWING = False
 SQRT_2 = math.sqrt(2)
+X_OFFSET = 400
+Y_OFFSET = 250
 
 def drawStroke(x,y,color):
     pg.draw.circle(screen, color, (x,y), STROKE_SIZE)
@@ -31,55 +34,55 @@ def drawStage(stage, color):
     match stage:
         case 1: # pole
             for i in range(200):
-                x = 425
-                y = 250 - i
+                x = X_OFFSET
+                y = Y_OFFSET - i
                 drawStroke(x,y,color)
         case 2: # truss
             for i in range(57):
-                x = 425 + i / SQRT_2
-                y = 90 - i / SQRT_2
+                x = X_OFFSET + i / SQRT_2
+                y = Y_OFFSET + 90 - i / SQRT_2
                 drawStroke(x,y,color)
         case 3: # beam
             for i in range(100):
-                x = 425 + i
-                y = 50
+                x = X_OFFSET + i
+                y = Y_OFFSET + 50
                 drawStroke(x,y,color)
         case 4: # rope
             for i in range(40):
-                x = 525
-                y = 50 + i
+                x = X_OFFSET + 125
+                y = Y_OFFSET + 50 + i
                 drawStroke(x,y,color)
         case 5: # head
             pg.time.delay(50)
-            for i in range(90,-271,-1):
+            for i in range(-90,271):
                 theta = i / 180 * math.pi
-                x = 525 + 25 * math.cos(theta)
-                y = 115 + 25 * math.sin(theta)
+                x = X_OFFSET + 125 + 25 * math.cos(theta)
+                y = Y_OFFSET - 135 + 25 * math.sin(theta)
                 drawStroke(x,y,color)
         case 6: # trunk
             for i in range(60):
-                x = 525
-                y = 140 + i
+                x = X_OFFSET + 125
+                y = Y_OFFSET - 110 + i
                 drawStroke(x,y,color)
         case 7: # left arm
             for i in range(50):
-                x = 525 - i / SQRT_2
-                y = 140 + i / SQRT_2
+                x = X_OFFSET + 125 - i / SQRT_2
+                y = Y_OFFSET - 110 + i / SQRT_2
                 drawStroke(x,y,color)
         case 8: # right arm
             for i in range(50):
-                x = 525 + i / SQRT_2
-                y = 140 + i / SQRT_2
+                x = X_OFFSET + 125 + i / SQRT_2
+                y = Y_OFFSET - 110 + i / SQRT_2
                 drawStroke(x,y,color)
         case 9: # left leg
             for i in range(50):
-                x = 525 - i / SQRT_2
-                y = 155 + i / SQRT_2
+                x = X_OFFSET + 125 - i / SQRT_2
+                y = Y_OFFSET - 110 + i / SQRT_2
                 drawStroke(x,y,color)
         case 10: # right leg
             for i in range(50):
-                x = 525 + i / SQRT_2
-                y = 155 + i / SQRT_2
+                x = X_OFFSET + 125 + i / SQRT_2
+                y = Y_OFFSET - 110 + i / SQRT_2
                 drawStroke(x,y,color)
 
 screen = pg.display.set_mode(SCREEN_SIZE)
